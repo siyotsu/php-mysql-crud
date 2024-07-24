bdbd7156bd20cc7842b40139b9dc31ae9e2257b7
Serhii Razlom <112971281+serhiib0x@users.noreply.github.com>
2024-07-24 21:36:22 +0300

    Update script.sql for correct  deploy into image of mysql:8.0

--- database/script.sql
+++ database/script.sql
@@ -1,12 +1,12 @@
-CREATE DATABASE php_mysql_crud;
+CREATE DATABASE IF NOT EXISTS php_mysql_crud;
 
-use php_mysql_crud;
+USE php_mysql_crud;
 
-CREATE TABLE task(
-  id INT(11) PRIMARY KEY AUTO_INCREMENT,
-  title VARCHAR(255) NOT NULL,
-  description TEXT,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS task (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DESCRIBE task;
