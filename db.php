4c3372627519892196fcfca23d01fd2104919441
siyotsu <109180406+siyotsu@users.noreply.github.com>
2024-08-16 21:41:57 +0300

    Update db.php

--- db.php
+++ db.php
@@ -2,11 +2,11 @@
 session_start();
 
 $host = getenv('MYSQL_HOST');
-$user = getenv('MYSQL_USER');
-$pass = getenv('MYSQL_PASSWORD');
+$root_user = getenv('MYSQL_ROOT_USER');
+$root_password = getenv('MYSQL_ROOT_PASSWORD');
 $db   = getenv('MYSQL_DATABASE');
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $root_user, $root_password, $db);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
